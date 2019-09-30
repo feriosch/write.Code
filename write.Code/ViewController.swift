6aca2d31de80bb43dd2355a48772321d95e1f129
Kims <A01020706@itesm.mx>
2019-09-30 14:47:59 -0500

    Initial Commit

---
+++ write.Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  write.Code
+//
+//  Created by cdt307 on 9/30/19.
+//  Copyright © 2019 FernandoRios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
